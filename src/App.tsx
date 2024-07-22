1eefca133a77b8e753dc6bf4b6568acee7a490fa
Ericsson <114271127+EricssonXD@users.noreply.github.com>
2024-07-22 10:02:51 +0800

    hash router to fix github page 404

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { useMemo } from 'react';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter, createHashRouter } from 'react-router-dom';
 import { routes } from './routes';
 
 function App() {
-  const router = useMemo(() => createBrowserRouter(routes, {
+  // const router = useMemo(() => createBrowserRouter(routes, {
+  //   basename: "/resume/",
+  // }), []);
+
+  // Use createHashRouter instead of createBrowserRouter to avoid 404 errors when refreshing the page in a GitHub Pages environment
+  const router = useMemo(() => createHashRouter(routes, {
     basename: "/resume/",
   }), []);
 
